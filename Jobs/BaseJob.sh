d3472395d9c3d76a544d3889403cf850f279cfdd
DavidKrGH <krumm.log@gmx.de>
2024-06-03 01:05:40 +0200

    Add DockerHandler

--- Jobs/BaseJob.sh
+++ Jobs/BaseJob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# BackupScripts version 1.0.3
+# BackupScripts version 2.0.0
 #################################### License ####################################
 # MIT License Copyright (c) 2023 David Krumm                                    #
 # All rights reserved.                                                          #
@@ -10,13 +10,15 @@
 #================================ BackupScripts ================================#
 #################################################################################
                                                                                 #     
-JOB_NAME="ChangeMeToUniqueName"                                                 # Unique job name. Do not use space or underscore!!!
+unique_job_name="ChangeMeToUniqueName"                                          # Unique job name. Do not use space or underscore!!!
                                                                                 #
-HOME_PATH="/PATH/TO/BackupScripts"                                              # Path to BackupScripts directory. Does not support Docker volume propagation!
+path_to_BackupScripts="/PATH/TO/BackupScripts"                                  # Path to BackupScripts directory. Does not support Docker volume propagation!
                                                                                 #
-hostname="HOSTNAME"                                                             # Name to identify your System in Snapshots
+system_id_name="HOSTNAME"                                                       # Name to identify your System in Snapshots
                                                                                 #
-notification_after_completion="false"                                           # (true/false) The notification system must be set up. See documentation.
+schedule_update_Restic_and_Rclone="monthly: 1"                                  #
+                                                                                #
+notify_after_completion="false"                                                 # (true/false) The notification system must be set up. See documentation.
                                                                                 #
 ############################## Restic ###########################################
                                                                                 #
@@ -24,15 +26,15 @@ notification_after_completion="false"                                           
 schedule_backup="always"                                                        # "never", "always", "weekly: Mon, Tue, Wed, Thu, Fri, Sat, Sun", "monthly: 15"
                                                                                 # Executed only once per day if the weekly or monthly pattern is used.
                                                                                 #
-source="/PATH/TO/DATA:ro"                                                       # Source directory to be backed up. Support for Docker volume propagation! E.G. "/PATH/TO/DATA:rw,slave" ro=read-only, rw=read-write 
+path_to_the_directory_to_be_backed_up="/PATH/TO/DATA:ro"                        # Source directory to be backed up. Support for Docker volume propagation! E.G. "/PATH/TO/DATA:rw,slave" ro=read-only, rw=read-write 
                                                                                 #
-repo="/PATH/TO/REPO:rw,slave"                                                   # Path to the backup repository. Support for Docker volume propagation! E.G. "/PATH/TO/REPO:rw,slave" ro=read-only, rw=read-write 
+path_to_restic_repository="/PATH/TO/REPO:rw,slave"                              # Path to the backup repository. Support for Docker volume propagation! E.G. "/PATH/TO/REPO:rw,slave" ro=read-only, rw=read-write 
                                                                                 #
-password_file="restic-repo.password"                                            # File in Config/ResticConfig. Insert yor repository password first.
+name_restic_password_file="restic-repo.password"                                # File in Config/ResticConfig. Insert yor repository password first.
                                                                                 #
-tags="--tag FirstTag --tag SecondTag"                                           # Tags to be applied to the backup snapshots
+restig_backup_tags="--tag FirstTag --tag SecondTag"                             # Tags to be applied to the backup snapshots
                                                                                 #
-filter_file="DefaultResticFilter.txt"                                           # Filter file to exclude specific files or directories from the backup
+name_restic_filter_file="DefaultResticFilter.txt"                               # Filter file to exclude specific files or directories from the backup
                                                                                 #
 restic_options=""                                                               # Additional options specific to Restic
                                                                                 #
@@ -41,11 +43,11 @@ restic_options=""                                                               
 schedule_forget="always"                                                        # "never", "always", "weekly: Mon, Tue, Wed, Thu, Fri, Sat, Sun", "monthly: 15"
                                                                                 # Executed only once per day if the weekly or monthly pattern is used.
                                                                                 #
-hourly_for="12h"                                                                # Number of hours to keep hourly snapshots
-daily_for="7d"                                                                  # Number of days to keep daily snapshots
-weekly_for="3m"                                                                 # Number of weeks to keep weekly snapshots
-monthly_for="1y"                                                                # Number of months to keep monthly snapshots
-yearly_for="5y"                                                                 # Number of years to keep yearly snapshots
+keep_hourly_for="12h"                                                           # Number of hours to keep hourly snapshots
+keep_daily_for="7d"                                                             # Number of days to keep daily snapshots
+keep_weekly_for="3m"                                                            # Number of weeks to keep weekly snapshots
+keep_monthly_for="1y"                                                           # Number of months to keep monthly snapshots
+keep_yearly_for="5y"                                                            # Number of years to keep yearly snapshots
                                                                                 #
 ############################## Prune                                            #
                                                                                 # Schedule for the execution of the Restic forget process:
@@ -53,28 +55,46 @@ schedule_prune="weekly: Mon, Wed, Sat"                                          
                                                                                 # Executed only once per day if the weekly or monthly pattern is used.
 ############################## Docker ###########################################
                                                                                 #
-handle_docker="false"                                                           # (true/false) Whether to handle Docker containers during backup
+name_docker_config_file="DockerExampleConfig.txt"                               #
                                                                                 #
-reverse_on_start="true"                                                         # (true/false) Whether to reverse the order of container_list on startup
-                                                                                #
-stop_start_remaining_container="false"                                          # (true/false) Whether to stop and start remaining containers after the containers in container_list have stopped
-                                                                                #
-container_list=("1. Container" "2. Container" "3. Container")                   # List of containers to be stopped in a specified order. Stopping takes place in the order of the list
+reverse_order_at_start="false"                                                  #
                                                                                 #
 ############################## Rclone ###########################################
                                                                                 #
 schedule_rclone="monthly: 10"                                                   # Schedule for running Rclone: "never", "always", "weekly: Mon, Tue, Wed, Thu, Fri, Sat, Sun", "monthly: 15" 
                                                                                 # Executed only once per day if the weekly or monthly pattern is used.
                                                                                 #
-dest_remote="RemoteName:/PATH/ON/REMOTE"                                        # Destination remote for Rclone. Does not support Docker volume propagation!
+rclone_remote_path="RemoteName:/PATH/ON/REMOTE"                                 # Destination remote for Rclone. Does not support Docker volume propagation!
                                                                                 #
-log_level="INFO"                                                                # Log level for Rclone: "DEBUG", "INFO", "NOTICE", "ERROR"
+name_rclone_filter_file="DefaultRcloneFilter.txt"                               # Filter file to exclude specific files or directories from the backup to remote
                                                                                 #
-rclone_options=""                                                               # Additional options specific to Restic
+rclone_log_level="INFO"                                                         # Log level for Rclone: "DEBUG", "INFO", "NOTICE", "ERROR"
+                                                                                #
+rclone_options=""                                                               # Additional options specific to Rclone
                                                                                 #
 #################################################################################
 
               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -85,60 +105,77 @@ rclone_options=""                                                               
 #                                                                               #
 #################################################################################
 
+############################# Setup Paths #######################################
 
+export JOB_NAME="$unique_job_name"
+export HOME_PATH="$path_to_BackupScripts"
+export SOURCE="$path_to_the_directory_to_be_backed_up"
+export REPOSITORY="$path_to_restic_repository"
+export REMOTE="$rclone_remote_path"
 
-########################## Setup File Paths #####################################
+export DOCKER_CONFIG="$HOME_PATH/Config/DockerConfig/$name_docker_config_file"
+export RESTIC_PW="$HOME_PATH/Config/RepositoryPassword/$name_restic_password_file"
+export RESTIC_FILTER="$HOME_PATH/Config/FilterConfig/ResticFilter/$name_restic_filter_file"
+export RCLONE_FILTER="$HOME_PATH/Config/FilterConfig/RcloneFilter/$name_rclone_filter_file"
 
 NOTIFIER="$HOME_PATH/Executor/Notifier"
 ConditionHandler="$HOME_PATH/Executor/ConditionHandler"
+DockerHandler="$HOME_PATH/Executor/DockerHandler"
 ResticBackupExec="$HOME_PATH/Executor/ResticBackupExec"
 ResticForgetExec="$HOME_PATH/Executor/ResticForgetExec"
 ResticPruneExec="$HOME_PATH/Executor/ResticPruneExec"
 RcloneExec="$HOME_PATH/Executor/RcloneExec"
 
-############################## Export Env Variables #############################
-
-export HOME_PATH JOB_NAME NOTIFIER
-
 ############################## Check Preconditions ##############################
 
-preconditions=0
-check_executable() {
-    local file=$1
-    if [ ! -f "$file" ]; then
-        echo "ERROR: $file does not exist."
-        preconditions=1
-    elif [ ! -x "$file" ]; then
-        echo "ERROR: $file exists but is not executable."
-        echo "-> Read the instructions and the setup files in the 'BackupScripts/SetupInstruction' directory."
-        echo "-> Use 'chmod +x $file' to give the file the required rights."
+directory_pahts=("$HOME_PATH" "$SOURCE" "$REPOSITORY")
+config_files=("$DOCKER_CONFIG" "$RESTIC_PW" "$RESTIC_FILTER" "$RCLONE_FILTER")
+executable_files=("$NOTIFIER" "$ConditionHandler" "$DockerHandler" "$ResticBackupExec" "$ResticForgetExec" "$ResticPruneExec" "$RcloneExec")
 
-        preconditions=1
-    fi
-}
-
-check_directory_exists() {
-    local directory=$(echo "$1" | cut -d':' -f1)
+for directory in "${directory_pahts[@]}"; do
+    directory=$(echo "$directory" | cut -d':' -f1)
+    # Check directrie exists
     if [ ! -d "$directory" ]; then
-    echo "ERROR: The directory $directory does not exist."
-    preconditions=1
+        echo "ERROR: The directory $directory does not exist."
+        exit 1
     fi
-}
+done
 
-check_directory_exists "$HOME_PATH"
-check_directory_exists "$source"
-check_directory_exists "$repo"
+for file in "${config_files[@]}"; do
+    # Check file exists
+    if [ ! -f "$file" ]; then
+        echo "ERROR: The directory $file does not exist."
+        exit 1
+    
+    # Check linebrake at file end
+    elif [ "$(tail -c 1 "$file"; echo x)" != "$(echo x)" ] && [ -n "$(tail -n 1 "$file")" ]; then
+        echo >> "$file"
+    fi
+done
 
-check_executable "$NOTIFIER"
-check_executable "$ConditionHandler"
-check_executable "$ResticBackupExec"
-check_executable "$ResticForgetExec"
-check_executable "$ResticPruneExec"
-check_executable "$RcloneExec"
+for file in "${executable_files[@]}"; do
 
-if [[ $preconditions != 0 ]]; then
-    exit 1
-fi
+    # Check executable exists
+    if [ ! -f "$file" ]; then
+        echo "ERROR: $file does not exist"
+        exit 1
+    
+    # Check executable file is executable
+    elif [ ! -x "$file" ]; then
+        echo "ERROR: $file exists but is not executable"
+
+        chmod +x "$script"
+        # Try to provide rights
+        if [ $? -eq 0 ]; then
+            echo "$file was provided with execution rights"
+        else
+            echo "ERROR: The attempt to assign execution rights to the $file failed."
+            echo "-> Read the instructions and the setup files in the 'BackupScripts/SetupInstruction' directory"
+            echo "-> Use 'sudo chmod +x $file' to give the file the required rights"
+            exit 1
+        fi
+    fi
+done
 
 ############################## Jobs #############################################
 
@@ -154,6 +191,27 @@ elif [[ "$job_is_already_executed" == 1 ]]; then
     exit 1                                          # Something went wrong
 fi
 
+############################## Update Restic & Rclone 
+
+# Evaluate schedule
+$ConditionHandler --task "evaluate" --type "lock" --process "Update" --schedule "$schedule_update_Restic_and_Rclone"
+evaluation=$?
+
+if [[ "$evaluation" == 0 ]]; then
+    $DockerHandler --action "update"
+fi
+
+############################## Stop Docker 
+
+$DockerHandler --action "stop"
+stop_exit_code=$?
+
+# If evaluation or ResticForgetExec fails
+if [[ "$stop_exit_code" == 1 ]]; then
+    $ConditionHandler --task "release" --type "execution"
+    exit 1
+fi
+
 ############################## Backup 
 
 # Evaluate schedule
@@ -161,8 +219,7 @@ $ConditionHandler --task "evaluate" --type "lock" --process "Backup" --schedule 
 evaluation=$?
 
 if [[ "$evaluation" == 0 ]]; then
-    $ResticBackupExec "$hostname" "$source" "$repo" "$password_file" "$filter_file" "$tags" "$restic_options" \
-    "$handle_docker" "$stop_start_remaining_container" "$reverse_on_start" "${container_list[@]}"
+    $ResticBackupExec "$system_id_name" "$restig_backup_tags" "$restic_options"
     backup_exit_code=$?
 fi
 
@@ -172,19 +229,30 @@ if [[ "$evaluation" == 1 || "$backup_exit_code" == 1 ]]; then
     exit 1
 fi
 
+############################## Start Docker
+
+$DockerHandler --action "start" --reverse "$reverse_order_at_start"
+start_exit_code=$?
+
+# If evaluation or ResticForgetExec fails
+if [[ "$start_exit_code" == 1 ]]; then
+    $ConditionHandler --task "release" --type "execution"
+    exit 1
+fi
+
 ############################## Forget
 
 # Kombine keep rules
-keep_rules="--keep-within-hourly $hourly_for --keep-within-daily $daily_for"
-keep_rules+=" --keep-within-weekly $weekly_for --keep-within-monthly $monthly_for"
-keep_rules+=" --keep-within-yearly $yearly_for"
+keep_rules="--keep-within-hourly $keep_hourly_for --keep-within-daily $keep_daily_for"
+keep_rules+=" --keep-within-weekly $keep_weekly_for --keep-within-monthly $keep_monthly_for"
+keep_rules+=" --keep-within-yearly $keep_yearly_for"
 
 # Evaluate schedule
 $ConditionHandler --task "evaluate" --type "lock" --process "Forget" --schedule "$schedule_forget"
 evaluation=$?
 
 if [[ "$evaluation" == 0 ]]; then
-    $ResticForgetExec "$repo" "$password_file" "$keep_rules"
+    $ResticForgetExec "$keep_rules"
     forget_exit_code=$?
 fi
 
@@ -201,7 +269,7 @@ $ConditionHandler --task "evaluate" --type "lock" --process "Prune" --schedule "
 evaluation=$?
 
 if [[ "$evaluation" == 0 ]]; then
-    $ResticPruneExec "$repo" "$password_file"
+    $ResticPruneExec
     prune_exit_code=$?
 fi
 
@@ -219,7 +287,7 @@ evaluation=$?
 
 if [[ "$evaluation" == 0 ]]; then
     $RcloneExec "$repo" "$dest_remote" "" \
-    "--log-file /LogFiles/${JOB_NAME}_$(date +'%Y-%m').log --log-level=$log_level" "$rclone_options"
+    "--log-file /LogFiles/${JOB_NAME}_$(date +'%Y-%m').log --log-level=$rclone_log_level" "$rclone_options"
     rclone_exit_code=$?
 fi
 
@@ -234,7 +302,7 @@ fi
 $ConditionHandler --task "release" --type "execution"
 
 completion_channel="file"
-if [[ "$notification_after_completion" = "true" ]]; then
+if [[ "$notify_after_completion" = "true" ]]; then
     completion_channel="system"
 fi
 $NOTIFIER --channel "file"
